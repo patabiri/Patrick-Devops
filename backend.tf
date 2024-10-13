21af8ad92c22cc789286de6f4ab7b930a4d44dc0
patabiri <patabiri99@gmail.com>
2024-10-13 01:15:21 -0600

    updated

--- backend.tf
+++
@@ -1,7 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket = "terra-statefile-afcon"
-    region = "us-east-1"
-    key    = "terraform/multi-resources"
-  }
-}
--- web.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-sudo yum install wget unzip httpd -y
-sudo systemctl start httpd
-sudo systemctl enable httpd
-sudo wget https://www.tooplate.com/zip-templates/2117_infinite_loop.zip
-sudo unzip -o 2117_infinite_loop.zip
-sudo cp -r 2117_infinite_loop/* /var/www/html/
-sudo systemctl restart httpd
